93aa73d8e7b37ee1a01af33f6185caef5990dd30
Bernard G. Tapiru, Jr. <tapirua@gmail.com>
2026-04-15 17:53:52 +0800

    chore: setup husky + lint-staged pre-commit hooks

---
+++ .husky/_/husky.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+if [ -z "$husky_skip_init" ]; then
+  debug () {
+    if [ "$HUSKY_DEBUG" = "1" ]; then
+      echo "husky (debug) - $1"
+    fi
+  }
+
+  readonly hook_name="$(basename "$0")"
+  debug "running $hook_name"
+  readonly git_params="$*"
+  readonly exit_code=0
+fi
